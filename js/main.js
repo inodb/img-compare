e9dda5450b476fc8e6bbbb441e6a812089b20a26
Ino de Bruijn <ino@ino.pm>
2016-06-30 15:02:08 -0400

    Add title and text parameters

--- js/main.js
+++ js/main.js
@@ -9,12 +9,23 @@ var img1 = getURLParameterByName("img1");
 var img2 = getURLParameterByName("img2");
 var label1 = getURLParameterByName("label1"); 
 var label2 = getURLParameterByName("label2"); 
+var text = getURLParameterByName("text"); 
+var title = getURLParameterByName("title"); 
 
 if (img1 !== "" && img2 !== "") {
     $("#img1").attr("src", img1);
     $("#img2").attr("src", img2);
-    $("#img1").attr("data-label", label1);
-    $("#img2").attr("data-label", label2);
+
+    if (label1 !== "" && label2 !== "") {
+        $("#img1").attr("data-label", label1);
+        $("#img2").attr("data-label", label2);
+    }
+    if (text !== "") {
+        $("#text").html(text);
+    }
+    if (title !== "") {
+        $("#title").text(title);
+    }
 } else {
     $(".juxtapose").after("Set images to compare in URL e.g. ?img1=http://image1.com&img2=http://image2.com&label1=before&label2=after");
 }
